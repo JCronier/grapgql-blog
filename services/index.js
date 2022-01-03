1785112f3c7c3f94056300a7c21ded1f2ccdf2f6
fr3chtoa45t <jcronier777@gmail.com>
2022-01-03 13:05:46 -0800

    Postcard shows all posts

---
+++ services/index.js
@@ -0,0 +1,61 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              id
+              name
+              bio
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql `
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_asc
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
